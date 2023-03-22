096db291681545614cc242d3ffd00fcf2a2574a6
ericpkatz <ericpkatz@gmail.com>
2023-03-22 11:15:01 -0400

    working app

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/profs_cms_db');
 
 const app = express();
 
@@ -7,8 +9,40 @@ app.use('/dist', express.static('dist'));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
+app.get('/api/content', async(req, res, next)=> {
+  try {
+    const response = await client.query('SELECT * from content');
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> {
-  console.log(`listening on port ${port}`);
+app.listen(port, async()=> {
+  try {
+    console.log(`listening on port ${port}`);
+    await client.connect();
+    let SQL = `
+      DROP TABLE IF EXISTS content;
+      CREATE TABLE content(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100),
+        description TEXT
+      );
+    `;
+    await client.query(SQL);
+    SQL = `
+      INSERT INTO content(title, description) VALUES('contents 1', 'desc 1');
+      INSERT INTO content(title, description) VALUES('contents 2', 'desc 2');
+      INSERT INTO content(title, description) VALUES('contents 3', 'desc 3');
+    `;
+    await client.query(SQL);
+    console.log('data is seeded');
+  }
+  catch(ex){
+    console.log(ex);
+  }
 });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import axios from 'axios';
+const root = createRoot(document.querySelector('#root'));
+
+const App = ()=> {
+  const [content, setContent] = useState([]);
+  useEffect(()=> {
+    const fetchContent = async()=> {
+      const response = await axios.get('/api/content');
+      setContent(response.data);
+    };
+    fetchContent();
+  }, []);
+  return (
+    <div>
+      <h1>Profs CMS ({ content.length })</h1>
+      <ul>
+        {
+          content.map( item => {
+            return (
+              <li key={ item.id }>
+                { item.title }
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+root.render(<App />);
