6846e997ac15348a9395160c94b665e100865938
ericpkatz <ericpkatz@gmail.com>
2023-03-22 11:59:52 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
+import { Link, HashRouter, Routes, Route, useParams } from 'react-router-dom';
 const root = createRoot(document.querySelector('#root'));
+
+const Content = ({ content })=> {
+  const { id } = useParams();
+  const item = content.find( item => item.id === id*1);
+  if(!item){
+    return null;
+  }
+  return (
+    <div>
+      <h2>{ item.name }</h2>
+      <p>
+        { item.description }
+      </p>
+    </div>
+
+  );
+};
 
 const App = ()=> {
   const [content, setContent] = useState([]);
@@ -14,20 +32,25 @@ const App = ()=> {
   }, []);
   return (
     <div>
-      <h1>Profs CMS ({ content.length })</h1>
+      <h1><Link to='/'>Profs CMS ({ content.length })</Link></h1>
       <ul>
         {
           content.map( item => {
             return (
               <li key={ item.id }>
+                <Link to={`/content/${item.id}`}> 
                 { item.title }
+                </Link>
               </li>
             );
           })
         }
       </ul>
+      <Routes>
+        <Route path='/content/:id' element={ <Content content={ content }/> } />
+      </Routes>
     </div>
   );
 };
 
-root.render(<App />);
+root.render(<HashRouter><App /></HashRouter>);
